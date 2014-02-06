5a7d5b275a38e9b05b9523b7e2829c51ddf2165d
Mikhail Bautin <mbautin@gmail.com>
2014-02-05 23:02:56 -0800

    Add YARD options and some documentation

---
+++ README.md
@@ -0,0 +1,28 @@
+## ssh-exec
+
+`ssh-exec` is a wrapper around Net::SSH based on
+[http://stackoverflow.com/questions/3386233/how-to-get-exit-status-with-rubys-netssh-library/3386375#3386375](this)
+StackOverflow answer, allowing to easily capture standard output, standard error, and the exit code
+of a command executed over [Net::SSH](https://github.com/net-ssh/net-ssh).
+
+### Examples
+
+```ruby
+require 'net/ssh'
+require 'ssh-exec'
+
+Net::SSH.start('somehost', 'someuser') do |ssh|
+  result = SshExec.ssh_exec!(ssh, 'echo I am remote host')
+  puts result.stdout  # "I am remote host"
+  puts result.stderr  # ""
+  puts result.exit_status  # 0
+
+  result = SshExec.ssh_exec!(ssh, 'false')
+  puts result.exit_status  # 1
+end
+```
+
+### License
+
+[http://www.apache.org/licenses/LICENSE-2.0.html](Apache License, Version 2.0)
+
--- lib/ssh-exec.rb
+++ lib/ssh-exec.rb
@@ -1,3 +1,5 @@
+# @markup markdown
+
 require 'net/ssh'
 require 'ostruct'
 require 'logger'
@@ -16,12 +18,15 @@ module SshExec
     end
   end
 
-  # Execute the given command using the given ssh object and
-  # @param ssh the Net::SSH object connected to a host
-  # @param options [Hash] optional flags:
-  #   :echo_stdout - whether to echo stdout from the subcommand
-  #   :echo_stderr - whether to echo stderr from the subcommand
-  # @return [OpenStruct] a struct containing :stdout, :stderr, :exit_status, and :exit_signal
+  # Execute the given command using the given ssh object and capture its standard output, standard
+  # error, and exit status. The implementation is based on this
+  # {http://stackoverflow.com/questions/3386233/how-to-get-exit-status-with-rubys-netssh-library/3386375#3386375
+  # StackOveflow answer}.
+  # @param ssh the {Net::SSH} shell to run the command in
+  # @param options [Hash] optional settings:
+  #   `echo_stdout` - whether to echo standard output from the subcommand,
+  #   `echo_stderr` - whether to echo standard error from the subcommand
+  # @return [OpenStruct] a struct containing `stdout`, `stderr`, `exit_status`, and `exit_signal`
   def self.ssh_exec!(ssh, command, options = {})
     options = options.clone
     echo_stdout = options[:echo_stdout]
@@ -65,19 +70,29 @@ module SshExec
     )
   end
 
-  def self.ensure_exec(ssh, command)
-    result = ssh_exec!(ssh, command)
+  # Runs the given command in the given SSH shell, and raises {ExecutionError}
+  # in case of failure (nonzero exit status). Logs the command in all cases.
+  # Logs the command, exit status, standard output and standard error in case of failure.
+  # @param ssh the {Net::SSH} shell to run the command in
+  # @param options [Hash] optional settings:
+  #   `echo_stdout` - whether to echo stdout from the subcommand,
+  #   `echo_stderr` - whether to echo stderr from the subcommand,
+  #   `quiet` - to suppress logging the command and the error in case of non-zero exit status
+  # @return [OpenStruct] a struct containing `stdout`, `stderr`, `exit_status`, and `exit_signal`
+  def self.ensure_exec(ssh, command, options = {})
+    result = ssh_exec!(ssh, command, options)
 
-    if command.split("\n").length == 1
-      @@log.info("Running on #{ssh.host}: #{command}")
-    end
+    options = options.clone
+    quiet = options.delete(:quiet)
+
+    @@log.info("Running on #{ssh.host}: #{command}") unless quiet
 
     if result.exit_status  != 0
       @@log.error(
         ("Failed running command #{command}: exit status #{result.exit_status}. " +
         (result.stdout.empty? ? "" : "Standard output:\n#{result.stdout}\n") +
         (result.stderr.empty? ? "" : "Standard error:\n#{result.stderr}")).strip
-      )
+      ) unless quiet
       raise ExecutionError.new(
         "Failed running command #{command}: exit status #{result.exit_status}"
       )
